403f1966abcd1617ded1fa99c6b750322c64a06e
xwilna <ilyanaderi69@gmail.com>
2025-04-29 16:10:32 +0330

    Delete 07.py

--- 07.py
+++
@@ -1,7 +0,0 @@
-from sklearn.impute import SimpleImputer, KNNImputer
-import numpy as np
-
-imputer = SimpleImputer(missing_values=np.nan, strategy='mean')
-
-X = np.array([1,0, np.nan, 1, 1]).reshape(-1,1)
-print(imputer.fit_transform(X))
